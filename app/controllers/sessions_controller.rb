b7a25663636b70b41b28105734deb919e596819a
Drew Thompson <drewallynthompson@gmail.com>
2020-12-07 09:21:09 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,13 @@ class SessionsController < ApplicationController
   
   def new 
   end
+  
+  def create
+    if params[:name].nil? || params[:name].empty?
+      redirect_to :login
+    else
+      session[:name] = params[:name]
+      redirect_to :welcome
+    end
+  end
 end
