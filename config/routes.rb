6249665b0b6bfb5f07b59c28adfe6603efe32f7f
Drew Thompson <drewallynthompson@gmail.com>
2020-12-07 09:12:04 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root '/login'
-  post '/login' => ''
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+  get '/show' => 'secrets#show'
 end
